3f4a9044c3bb7b9b9f24f32b005e582d5a07d725
buitienthanhnt@gmail.com <namtanvietnam>
2024-06-04 11:42:38 +0700

    dev: update

--- index.php
+++ index.php
@@ -2,8 +2,39 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-use function Stringy\create as s;
+// use function Stringy\create as s;
+// echo(s('fòô     bàř')->collapseWhitespace()->swapCase());
 
-// echo($_SERVER['HTTP_CLIENT_IP'] ?? $_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR']);
+use Elastic\Elasticsearch\ClientBuilder;
 
-echo(s('fòô     bàř')->collapseWhitespace()->swapCase());
+$client = ClientBuilder::create()->build();
+print_r($client->info());
+
+exit();
+
+// // Index a document
+// $params = [
+//     'index' => 'my_index',
+//     'type' => 'my_type',
+//     'id' => 'my_id',
+//     'body' => ['testField' => 'abc']
+// ];
+
+// $response = $client->index($params);
+// print_r($response);
+
+// // Search for a document
+// $params = [
+//     'index' => 'my_index',
+//     'type' => 'my_type',
+//     'body' => [
+//         'query' => [
+//             'match' => [
+//                 'testField' => 'abc'
+//             ]
+//         ]
+//     ]
+// ];
+
+// $response = $client->search($params);
+// print_r($response);
